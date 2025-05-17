17315b95e98f561cddf09d62a7959b91f0545d44
kasibo <clarksondavid348@gmail.com>
2025-05-17 21:25:20 +0100

    first commit

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,11 +1,19 @@
-// Footer year and last modified
-document.getElementById("year").textContent = new Date().getFullYear();
-document.getElementById("lastModified").textContent = document.lastModified;
+document.addEventListener('DOMContentLoaded', function() {
+    // Set footer copyright year
+    const currentYear = new Date().getFullYear();
+    console.log(currentYear);
+    document.getElementById('currentyear').innerText = currentYear;
 
-// Hamburger menu toggle
-const menuBtn = document.getElementById("menu");
-const navMenu = document.getElementById("navMenu");
+    // Set footer last modified date
+    document.getElementById('lastModified').innerText = document.lastModified;
 
-menuBtn.addEventListener("click", () => {
-  navMenu.style.display = navMenu.style.display === "flex" ? "none" : "flex";
-});
+    // Add event listener to the menu button
+    const hamButton = document.querySelector('#menu');
+    const navigation = document.querySelector('.navigation');``
+
+    hamButton.addEventListener('click', function() {
+        navigation.classList.toggle('open');
+        hamButton.classList.toggle('open');
+    });
+
+    });
