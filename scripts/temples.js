184b4df32f77251c3b4a7d8f55d12d0480f6427a
kasibo <clarksondavid348@gmail.com>
2025-05-24 12:07:47 +0100

    first commit

---
+++ scripts/temples.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Set footer copyright year
+    const currentYear = new Date().getFullYear();
+    console.log(currentYear);
+    document.getElementById('currentyear').innerText = currentYear;
+
+    // Set footer last modified date
+    document.getElementById('lastModified').innerText = document.lastModified;
+
+    // Add event listener to the menu button
+    const hamButton = document.querySelector('#menu');
+    const navigation = document.querySelector('.navigation');``
+
+    hamButton.addEventListener('click', function() {
+        navigation.classList.toggle('open');
+        hamButton.classList.toggle('open');
+    });
+
+    });
