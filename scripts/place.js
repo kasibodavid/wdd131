6e549c8138593d4ddd22767a47cdccebd6ec61dc
kasibo <clarksondavid348@gmail.com>
2025-05-27 00:50:57 +0100

    changes

--- scripts/place.js
+++ scripts/place.js
@@ -1,25 +1,20 @@
-// Get footer elements
-document.getElementById("year").textContent = new Date().getFullYear();
-document.getElementById("lastModified").textContent = document.lastModified;
+const currentYear = document.getElementById('currentyear');
+currentYear.textContent = new Date().getFullYear();
 
-// Wind Chill Calculation
-function calculateWindChill(tempF, speedMph) {
-  return (
-    35.74 +
-    0.6215 * tempF -
-    35.75 * Math.pow(speedMph, 0.16) +
-    0.4275 * tempF * Math.pow(speedMph, 0.16)
-  ).toFixed(2);
+const lastModified = document.getElementById('lastModified');
+lastModified.textContent = (document.lastModified);
+
+const temperature = 74
+const windSpeed = 14
+
+function calculateWindChill(temperature, windSpeed) {
+    if (temperature <= 50 && windSpeed > 3) {
+        const windChill = 35.74 + 0.6215 * temperature - 35.75 * (windSpeed ** 0.16) + 0.4275 * temperature * (windSpeed ** 0.16);
+        const roundedWindChill = Math.round(windChill * 100) / 100;
+        return roundedWindChill
+    } else {
+        return "N/A";
+    }
 }
-
-// Static values for now
-const temp = parseFloat(document.getElementById("temp").textContent);
-const wind = parseFloat(document.getElementById("wind").textContent);
-const windChillSpan = document.getElementById("windchill");
-
-// Conditions for calculating windchill
-if (temp <= 50 && wind > 3) {
-  windChillSpan.textContent = `${calculateWindChill(temp, wind)} °F`;
-} else {
-  windChillSpan.textContent = "N/A";
-}
+const windChillFactor = document.querySelector("#windchill-factor");
+windChillFactor.textContent = calculateWindChill(temperature, windSpeed);
