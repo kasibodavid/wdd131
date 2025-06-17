b55b900a01c256f0bfe79a512914c1e07b55aa2b
kasibo <clarksondavid348@gmail.com>
2025-06-17 12:35:09 +0100

    project

---
+++ project/scripts/script.js
@@ -0,0 +1,20 @@
+// scripts/script.js
+
+// ScrollReveal initialization
+ScrollReveal().reveal('.fade-down', {
+  delay: 300,
+  distance: '50px',
+  origin: 'top',
+  duration: 1000,
+  easing: 'ease-in-out',
+  reset: false
+});
+
+ScrollReveal().reveal('.fade-up', {
+  delay: 300,
+  distance: '50px',
+  origin: 'bottom',
+  duration: 1000,
+  easing: 'ease-in-out',
+  reset: false
+});
